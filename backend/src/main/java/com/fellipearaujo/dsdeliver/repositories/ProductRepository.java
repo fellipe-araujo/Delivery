a989b64d57b932043bb68c04e18d1ccd954f4d7b
Fellipe <araujo.fellipe@hotmail.com>
2021-01-06 16:19:52 -0300

    GET /products

---
+++ backend/src/main/java/com/fellipearaujo/dsdeliver/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.fellipearaujo.dsdeliver.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fellipearaujo.dsdeliver.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	List<Product> findAllByOrderByNameAsc();
+}
