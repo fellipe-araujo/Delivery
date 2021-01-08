b39f5e19d84bc9918f9783c9818075555db0c71d
Fellipe <araujo.fellipe@hotmail.com>
2021-01-08 14:15:11 -0300

    Add Footer.

--- front-web/src/components/Navbar/index.tsx
+++ front-web/src/components/Navbar/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import './styles.css';
-import { ReactComponent as Logo } from '../../assets/logo.svg';
+import React from "react";
+import "./styles.css";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 const Navbar = () => {
   return (
-		<nav className="main-navbar">
+    <nav className="main-navbar">
       <Logo />
-      <a href="home" className="logo-text">DS Delivery</a>
-		</nav>
-	);
+      <a href="home" className="logo-text">
+        DS Delivery
+      </a>
+    </nav>
+  );
 };
 
 export default Navbar;
