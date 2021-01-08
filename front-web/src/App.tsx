aba51efa9c7f533e6dac5f11e73d5104d0693e33
Fellipe <araujo.fellipe@hotmail.com>
2021-01-08 13:58:01 -0300

    Add Home page.

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
-function App() {
+const App = () => {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
